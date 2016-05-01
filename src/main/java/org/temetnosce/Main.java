97346132485c933fc02be1d38e7cbc1d40112460
Quinn Peters <quinn.t.peters@gmail.com>
2016-05-01 19:47:07 -0400

    I have a working verticle

--- src/main/java/org/temetnosce/Main.java
+++ src/main/java/org/temetnosce/Main.java
@@ -1,8 +1,21 @@
 package org.temetnosce;
 
-public class Main {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 
+public class Main extends AbstractVerticle {
+
+	public void start() {
+		vertx.createHttpServer().requestHandler( req -> {
+			req.response()
+			.putHeader( "content-type", "text/plain" )
+			.end( "Hello, I'm vertexing!" );
+		}).listen( 8000 );
+	}
+	
 	public static void main(String[] args) {
-		System.out.println( "Test" );
+		Main m = new Main();
+		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle( m );
 	}
 }
