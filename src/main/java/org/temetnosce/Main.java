b14391db36f2bb4dbe5f3258ee428bc204cd3333
Quinn Peters <quinn.t.peters@gmail.com>
2016-05-01 20:42:35 -0400

    Now using vert.x web

--- src/main/java/org/temetnosce/Main.java
+++ src/main/java/org/temetnosce/Main.java
@@ -2,15 +2,27 @@ package org.temetnosce;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
 
 public class Main extends AbstractVerticle {
 
 	public void start() {
-		vertx.createHttpServer().requestHandler( req -> {
-			req.response()
-			.putHeader( "content-type", "text/plain" )
-			.end( "Hello, I'm vertexing!" );
-		}).listen( 8000 );
+		HttpServer server = vertx.createHttpServer();
+		Router router = Router.router( vertx );
+		
+		Route route = router.route();
+		route.handler( rc -> {
+			HttpServerResponse hsr = rc.response();
+			hsr.putHeader( "content-type", "text/plain" );
+			hsr.end( "Vert.x is using WEB" );
+		});
+		
+		server.requestHandler( router::accept ).listen( 8000 );
+		
+		System.out.println( "Started webserver" );
 	}
 	
 	public static void main(String[] args) {
